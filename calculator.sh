19b8ab4f7344ab3016b6e1d163dc1450253fd5dc
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2024-02-02 17:18:24 +0530

    Update calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,25 @@
+# !/bin/bash
+# A simple calculator shell program
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+
+echo -n "Enter First Number: "
+read a
+echo -n "Enter Second Number: "
+read b
+echo -n "Enter the Choice: "
+read ch
+
+case $ch in
+  1)res=`expr $a + $b`
+  ;;
+  2)res=`expr $a - $b`
+  ;;
+  3)res=`expr $a \* $b`
+  ;;
+  4)res=`expr $a / $b`
+  ;;
+esac
+echo "Result : $res"
